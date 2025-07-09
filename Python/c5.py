9f67bc7e30ff7f2353b63bc9a939e1f7cbb1ffb8
Lecinel <lsh3000h@gmail.com>
2025-07-09 10:25:53 +0900

    missing add

--- Python/c5.py
+++ Python/c5.py
@@ -2,6 +2,8 @@ num1, num2, num3 = map(int,input("숫자 3개를 입력해주세요").split())
 if num1 > num2:
     if num1 > num3:
         print(num1)
+    else:
+        print(num3)
 elif num2 > num3:
     print(num2)
 else:
